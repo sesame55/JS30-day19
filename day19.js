e46abf85b226afe9a26d7eb37dfe6f9b8b696195
Sesame <mushroom0505@gmail.com>
2020-10-26 08:23:51 +0800

    位移測試
    位移測試

--- day19.js
+++ day19.js
@@ -121,9 +121,11 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
         // 使用位移
-        pixels.data[i - 150] = pixels.data[i + 0]; // RED
-        pixels.data[i + 1 + 500] = pixels.data[i + 1]; // GREEN
-        pixels.data[i + 2 - 550] = pixels.data[i + 2]; // Blue
+        // pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        // pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        // pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+        // 測試
+        pixels.data[i - (canvas.width / 4) * 1000] = pixels.data[i + 0]; // RED
     }
     return pixels;
 }
